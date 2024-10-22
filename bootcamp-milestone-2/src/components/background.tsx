3ebf36a1c37043986288fa1b933055b4fb9c6935
Matthew Blam <blammatthew@gmail.com>
2024-10-22 15:50:34 -0700

    feat(background): add cursor follow spotlight animation

--- bootcamp-milestone-2/src/components/background.tsx
+++ bootcamp-milestone-2/src/components/background.tsx
@@ -3,17 +3,19 @@ import { Lexend_Exa } from "next/font/google";
 import { twMerge } from "tailwind-merge";
 import { motion } from "framer-motion";
 
+import useMousePosition from "../utils/useMousePosition";
+
 const lexendExa = Lexend_Exa({ weight: "600", subsets: ["latin"] });
 
 const Pattern = ({ multiplier }: { multiplier: number }) => {
     const bottom = `${32 + 170 * multiplier}px`;
     return (
         <div
-            className="absolute -z-10 w-full h-fit left-12 flex"
+            className="horizontal flex justify-center absolute -z-10 w-[calc(100%-48px-48px)] h-fit left-12"
             style={{ bottom: bottom }}>
             <div
                 className={twMerge(
-                    "text-[160px] select-none text-nowrap text-slate-200/50 leading-none",
+                    "select-none text-nowrap leading-none",
                     lexendExa.className
                 )}>
                 Matthew Blam
@@ -22,39 +24,81 @@ const Pattern = ({ multiplier }: { multiplier: number }) => {
     );
 };
 
-const Background = () => {
+const Wallpaper = ({
+    mask,
+    x,
+    y,
+    fade,
+}: {
+    mask?: boolean;
+    x: number;
+    y: number;
+    fade: boolean;
+}) => {
     let amount = [];
     for (let i = 0; i < 21; i++) {
         amount.push(i);
     }
+
+    const size = 500;
+
     return (
-        <div id="background">
+        <motion.div
+            animate={{
+                WebkitMaskPosition: `${x - size / 2}px ${y - size / 2}px`,
+            }}
+            transition={{ type: "tween", ease: "backOut", duration: 0.3 }}
+            className={twMerge(
+                fade ? "text-slate-200/65" : "text-slate-300/50",
+                "w-full h-full absolute",
+                mask ? "mask" : ""
+            )}>
             {amount.map((multiplier) => {
                 return (
                     <Pattern multiplier={multiplier} key={multiplier}></Pattern>
                 );
             })}
-            <motion.div
-                animate={{
-                    backgroundPosition: ["0% 0%", "100% 100%", "0% 0%"],
-                }}
-                transition={{
-                    repeat: Infinity,
-                    duration: 10,
-                    times: [0, 0.5, 1],
-                }}
-                className="absolute bg-gradient-to-r to-transparent via-slate-100 from-transparent bg-size-200 bg-pos-0 w-full h-full -z-10"></motion.div>
-            <motion.div
-                animate={{
-                    backgroundPosition: ["0% 0%", "100% 100%", "0% 0%"],
-                }}
-                transition={{
-                    repeat: Infinity,
-                    duration: 8,
-                    delay: 2,
-                    times: [0, 0.5, 1],
-                }}
-                className="absolute bg-gradient-to-b to-transparent via-slate-100/40 from-transparent bg-size-200 bg-pos-0 w-full h-full -z-10"></motion.div>
+            {fade && (
+                <motion.div
+                    animate={{
+                        backgroundPosition: ["0% 0%", "100% 100%", "0% 0%"],
+                    }}
+                    transition={{
+                        repeat: Infinity,
+                        duration: 10,
+                        times: [0, 0.5, 1],
+                    }}
+                    className="absolute bg-gradient-to-r to-transparent via-slate-100 from-transparent bg-size-200 bg-pos-0 w-full h-full -z-10"></motion.div>
+            )}
+            {fade && (
+                <motion.div
+                    animate={{
+                        backgroundPosition: ["0% 0%", "100% 100%", "0% 0%"],
+                    }}
+                    transition={{
+                        repeat: Infinity,
+                        duration: 8,
+                        delay: 2,
+                        times: [0, 0.5, 1],
+                    }}
+                    className="absolute bg-gradient-to-b to-transparent via-slate-100 from-transparent bg-size-200 bg-pos-0 w-full h-full -z-10"></motion.div>
+            )}
+        </motion.div>
+    );
+};
+
+const Background = () => {
+    const { x, y } = useMousePosition();
+    return (
+        <div
+            id="background"
+            className="w-full h-full absolute top-0 left-0 -z-20">
+            <Wallpaper fade={true} x={50} y={50}></Wallpaper>
+            <Wallpaper
+                fade={false}
+                mask={true}
+                x={x ? x : 50}
+                y={y ? y : 50}></Wallpaper>
         </div>
     );
 };
--- bootcamp-milestone-2/src/components/navbar.tsx
+++ bootcamp-milestone-2/src/components/navbar.tsx
@@ -8,7 +8,7 @@ const Navbar = () => {
             className="h-24 bg-gradient-to-b from-slate-100 to-transparent from-80% to-95% overflow-scroll select-none flex gap-10 justify-around sm:justify-between items-center px-12 pt-10 pb-6">
             <Link
                 href="/"
-                className="cursor-pointer font-semibold text-2xl hover:scale-110 hidden sm:block">
+                className="cursor-pointer font-semibold text-2xl hover:scale-[113%] hidden sm:block">
                 mb
             </Link>
             <nav className="flex gap-8 md:gap-12 font-semibold text-base">
@@ -36,7 +36,7 @@ const Navbar = () => {
                 href="https://github.com/MatthewBlam"
                 target="_blank"
                 rel="noopener noreferrer"
-                className="cursor-pointer hover:scale-110 hidden sm:block">
+                className="cursor-pointer hover:scale-[113%] hidden sm:block">
                 <SiGithub className="size-[26px]"></SiGithub>
             </a>
         </header>
